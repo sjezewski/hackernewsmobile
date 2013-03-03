80c36c0d9de8d9c701105c7d858ed4398fa26193
Sean Jezewski <seanwjezewski@gmail.com>
2013-03-03 15:55:11 -0800

    Footer

--- scripts/sections/footer.ts
+++ scripts/sections/footer.ts
@@ -1,5 +1,13 @@
 $("./body") {
-   insert_bottom("footer")
+   insert_bottom("footer") {
+     move_here("//span[contains(@class,'yclinks')]") {
+       remove("./text()")
+     }
+   }
+
+   # Cleanup lower part of main table
+
+   remove(".//*[@bgcolor='#ff6600']/ancestor::table[1]/ancestor::tr[1]")
 
 
 }
